59ba9cefd13f4027184c820531d03c70b9e8ff5f
craig <craig.moir.01@gmail.com>
2023-06-15 17:23:10 +0100

    shuffling test loader

--- train_classification.py
+++ train_classification.py
@@ -135,7 +135,7 @@ def main(args):
     train_dataset = PipeNetDataLoader(root=data_path, args=args, split='train', process_data=args.process_data)
     test_dataset = PipeNetDataLoader(root=data_path, args=args, split='test', process_data=args.process_data)
     trainDataLoader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=10, drop_last=True)
-    testDataLoader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=10)
+    testDataLoader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True, num_workers=10)
 
     '''MODEL LOADING'''
     num_class = args.num_category
