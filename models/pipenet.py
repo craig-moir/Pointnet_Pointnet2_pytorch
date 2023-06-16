c2b56ac15bd3872a19c67d0033220980f347b28f
craig <craig.moir.01@gmail.com>
2023-06-16 14:27:58 +0100

    making individual nets for each property

--- models/pipenet.py
+++ models/pipenet.py
@@ -12,17 +12,43 @@ class get_model(nn.Module):
         self.sa1 = PointNetSetAbstractionMsg(512, [0.1, 0.2, 0.4], [16, 32, 128], in_channel,[[32, 32, 64], [64, 64, 128], [64, 96, 128]])
         self.sa2 = PointNetSetAbstractionMsg(128, [0.2, 0.4, 0.8], [32, 64, 128], 320,[[64, 64, 128], [128, 128, 256], [128, 128, 256]])
         self.sa3 = PointNetSetAbstraction(None, None, None, 640 + 3, [256, 512, 1024], True)
-        self.fc1 = nn.Linear(1024, 512)
-        self.bn1 = nn.BatchNorm1d(512)
-        self.drop1 = nn.Dropout(0.4)
-        self.fc2 = nn.Linear(512, 256)
-        self.bn2 = nn.BatchNorm1d(256)
-        self.drop2 = nn.Dropout(0.5)
-        self.fc3 = nn.Linear(256, num_class)
         
-        self.fc_r = nn.Linear(256, 1)
-        self.fc_n = nn.Linear(256, 3)
-        self.fc_d = nn.Linear(256, 3)
+        # classification
+        self.cl_fc1 = nn.Linear(1024, 512)
+        self.cl_bn1 = nn.BatchNorm1d(512)
+        self.cl_drop1 = nn.Dropout(0.4)
+        self.cl_fc2 = nn.Linear(512, 256)
+        self.cl_bn2 = nn.BatchNorm1d(256)
+        self.cl_drop2 = nn.Dropout(0.5)
+        self.cl_fc3 = nn.Linear(256, num_class)
+        
+        # direction
+        self.d_fc1 = nn.Linear(1024, 512)
+        self.d_bn1 = nn.BatchNorm1d(512)
+        self.d_drop1 = nn.Dropout(0.4)
+        self.d_fc2 = nn.Linear(512, 256)
+        self.d_bn2 = nn.BatchNorm1d(256)
+        self.d_drop2 = nn.Dropout(0.5)
+        self.d_fc3 = nn.Linear(256, 3)
+        
+        # normal
+        self.n_fc1 = nn.Linear(1024, 512)
+        self.n_bn1 = nn.BatchNorm1d(512)
+        self.n_drop1 = nn.Dropout(0.4)
+        self.n_fc2 = nn.Linear(512, 256)
+        self.n_bn2 = nn.BatchNorm1d(256)
+        self.n_drop2 = nn.Dropout(0.5)
+        self.n_fc3 = nn.Linear(256, 3)
+        
+        # radius
+        self.r_fc1 = nn.Linear(1024, 512)
+        self.r_bn1 = nn.BatchNorm1d(512)
+        self.r_drop1 = nn.Dropout(0.4)
+        self.r_fc2 = nn.Linear(512, 256)
+        self.r_bn2 = nn.BatchNorm1d(256)
+        self.r_drop2 = nn.Dropout(0.5)
+        self.r_fc3 = nn.Linear(256, 1)
+        
 
     def forward(self, xyz):
         B, _, _ = xyz.shape
@@ -34,33 +60,38 @@ class get_model(nn.Module):
         l1_xyz, l1_points = self.sa1(xyz, norm)
         l2_xyz, l2_points = self.sa2(l1_xyz, l1_points)
         l3_xyz, l3_points = self.sa3(l2_xyz, l2_points)
-        cl = l3_points.view(B, 1024)
-        cl = self.drop1(F.relu(self.bn1(self.fc1(cl))))
-        cl = self.drop2(F.relu(self.bn2(self.fc2(cl))))
-        cl = self.fc3(cl)
-        cl = F.log_softmax(cl, -1)
-
-        r = l3_points.view(B, 1024)
-        r = self.drop1(F.relu(self.bn1(self.fc1(r))))
-        r = self.drop2(F.relu(self.bn2(self.fc2(r))))
-        r = self.fc_r(r)
-        # print("r:", r)
-        # print("r size:", r.size())
         
+        # classification
+        cl = l3_points.view(B, 1024)
+        cl = self.cl_drop1(F.relu(self.cl_bn1(self.cl_fc1(cl))))
+        cl = self.cl_drop2(F.relu(self.cl_bn2(self.cl_fc2(cl))))
+        cl = self.cl_fc3(cl)
+        cl = F.log_softmax(cl, -1)
+        
+        # direction
         d = l3_points.view(B, 1024)
-        d = self.drop1(F.relu(self.bn1(self.fc1(d))))
-        d = self.drop2(F.relu(self.bn2(self.fc2(d))))
-        d = self.fc_d(d)
+        d = self.d_drop1(F.relu(self.d_bn1(self.d_fc1(d))))
+        d = self.d_drop2(F.relu(self.d_bn2(self.d_fc2(d))))
+        d = self.d_fc3(d)
         # print("d:", d)
         # print("d size:", d.size())
         
+        # normal
         n = l3_points.view(B, 1024)
-        n = self.drop1(F.relu(self.bn1(self.fc1(n))))
-        n = self.drop2(F.relu(self.bn2(self.fc2(n))))
-        n = self.fc_n(n)
+        n = self.n_drop1(F.relu(self.n_bn1(self.n_fc1(n))))
+        n = self.n_drop2(F.relu(self.n_bn2(self.n_fc2(n))))
+        n = self.n_fc3(n)
         # print("n:", n)
         # print("n size:", n.size())
         # print("n norm:", torch.norm(n, dim=1))
+
+        # radius
+        r = l3_points.view(B, 1024)
+        r = self.r_drop1(F.relu(self.r_bn1(self.r_fc1(r))))
+        r = self.r_drop2(F.relu(self.r_bn2(self.r_fc2(r))))
+        r = self.r_fc3(r)
+        # print("r:", r)
+        # print("r size:", r.size())
         # exit()
 
         return cl, d, n, r, l3_points
